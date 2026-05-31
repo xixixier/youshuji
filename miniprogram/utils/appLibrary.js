faf3aaaaead8b389d6a557e5395f27b786fc3742
xixixier <2992744026@qq.com>
2026-05-31 21:37:11 +0800

    fix: empty Infuse Pro iconUrl to force fallback to the custom high-fidelity flame SVG logo instead of Firecore's company FC logo

--- miniprogram/utils/appLibrary.js
+++ miniprogram/utils/appLibrary.js
@@ -769,7 +769,7 @@ const APP_LIBRARY = [
     category: "影音娱乐",
     defaultPrice: 10,
     brandColor: "#FF5E2B",
-    iconUrl: "https://api.companyenrich.com/logo/firecore.com",
+    iconUrl: "",
     iconSvg: `<svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'><rect width='24' height='24' rx='6' fill='%23FF5E2B'/><path d='M12 4.5s-4 4.5-4 8.5c0 2.2 1.8 4 4 4s4-1.8 4-4c0-4-4-8.5-4-8.5zm-1.5 6.5s-1.5 1.5-1.5 3c0 1 .8 1.8 1.8 1.8.8 0 1.2-.5 1.2-.5s-1.5-1-1.5-2.8c0-1.5 0-1.5 0-1.5z' fill='white'/></svg>`
   },
   {
